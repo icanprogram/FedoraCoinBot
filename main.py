7ed69ae4cca9090539a679d361bb89a74d9f5de2
Harry Peach <harrypeach1999@gmail.com>
2013-12-25 11:07:27 +0000

    Commented lines and added a string receive loop

--- main.py
+++ main.py
@@ -6,16 +6,25 @@ from random import randint
 
 HOST="irc.freenode.net"
 PORT=6667
-NICK="FedoraCoinBot420"
+NICK="FedoraMon"
 IDENT="CephonBot1"
 REALNAME="CephonBot"
 CHAN="#fedoracoin"
 readbuffer=""
 
-f = open('log.txt', 'w')
+f = open('log.txt', 'w') #Opening the log txt file
+
 s=socket.socket( )
 s.connect((HOST, PORT))
 s.send("NICK %s\r\n" % NICK)
 s.send("USER %s %s bla :%s\r!\n" % (IDENT, HOST, REALNAME))
 s.send("JOIN :%s\r\n" % CHAN)
-s.send("PRIVMSG %s :%s\r\n" % (CHAN, "I am FedoraCoinBot version Alpha 0.06. !help for info"))
+s.send("PRIVMSG %s :%s\r\n" % (CHAN, "I am FedoraCoinBot version Beta 1.0. !help for info"))
+
+# Loop
+while 1:
+ text=s.recv(2040)
+ print text # Print received text to console
+ f.write(text) # Write the text to file
+ if text.find('PING') !=-1: # This is to respond to the server when pinged, otherwise the bot will get kicked.
+	s.send("PONG %s\r\n")
